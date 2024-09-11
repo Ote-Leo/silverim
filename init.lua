ce6db86e18d97d6264f7e347c7059823c2f6793c
Ote Leo <ote.leo73@gmail.com>
2024-09-12 00:18:55 +0300

    Adding ruff_lsp as a default dependency

--- init.lua
+++ init.lua
@@ -475,6 +475,8 @@ require("lazy").setup({
 						["python.pythonPath"] = "python3",
 					},
 				},
+				ruff_lsp = {
+					autostart = true,
 				},
 				rust_analyzer = {},
 				lua_ls = {
@@ -501,6 +503,7 @@ require("lazy").setup({
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
 				"stylua", -- Used to format Lua code
+				"ruff_lsp",
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
